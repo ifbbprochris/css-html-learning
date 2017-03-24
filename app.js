cbc487ac74b0681a1595e384d443a49902499510
ChrisChou <chris@aeris.io>
2017-03-24 16:30:20 +0800

    add task two

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('*', function(req, res){
-  res.sendFile(__dirname + '/public/view/task-one.html');
+  res.sendFile(__dirname + '/public/view/task-two.html');
 });
 
-app.listen(4330);
+app.listen(4330,function(){
+    console.log('start');
+});
